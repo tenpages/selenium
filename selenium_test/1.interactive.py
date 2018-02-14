bd6233e1c3148eb67349c70e604520eaeccdd667
Shuo <tenpages66@163.com>
2018-02-13 23:35:53 -0500

    interactive keys

--- selenium_test/1.interactive.py
+++ selenium_test/1.interactive.py
@@ -1,13 +1,25 @@
+#!/usr/bin/env python3
+
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 import time
 
 browser = webdriver.Chrome()
-browser.get("http://www.taobao.com")
-input_str=browser.find_element_by_id('q')
-input_str=send_keys("ipad")
+browser.get("http://www.linkedin.com")
+input_str=browser.find_element_by_xpath("//input[@type='text'][@class='login-email']")
+input_str.send_keys("==your mail here==")
+input_str=browser.find_element_by_name("session_password")
+input_str.send_keys("==your pswd here==")
 time.sleep(1)
+input_str.send_keys(Keys.RETURN)
+print("logged in")
+
+time.sleep(5)
+print("redirecting")
+
+input_str=browser.find_element_by_xpath	("//input[@role='combobox'][@type='text']")
+print('found')
 input_str.clear()
-input_str.send_keys("Macbook pro")
-button = browser.find_element_by_class_name('btn-search')
-button.click()
+input_str.send_keys('Lisa')
+input_str.send_keys(Keys.RETURN)
